3f0f3532105865bd63a5320fa2765e4bd8826694
Manfred Diaz <takeitallsource@gmail.com>
2023-02-17 23:15:16 -0500

    Fixing Slurm request

--- src/static/bin/run_slurm.sh
+++ src/static/bin/run_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --partition=long-cpu-eek                           # Ask for unkillable job
-#SBATCH --cpus-per-task=32                                # Ask for 2 CPUs
+#SBATCH --partition=long-cpu-grace                           # Ask for unkillable job
+#SBATCH --cpus-per-task=12                                # Ask for 2 CPUs
 #SBATCH --gres=gpu:0                                     # Ask for 1 GPU
 #SBATCH --mem=64G                                        # Ask for 10 GB of RAM
 #SBATCH -o /network/scratch/d/diazcabm/slurm-%j.out  # Write the log on scratch
@@ -17,7 +17,7 @@ conda activate metacurriculum
 
 # 4. Launch your job, tell it to save the model in $SLURM_TMPDIR
 #    and look for the dataset into $SLURM_TMPDIR
-python main.py --outdir /network/scratch/d/diazcabm/logs/ccgm
+python main.py --outdir $SLURM_TMPDIR/static 
 
 # 5. Copy whatever you want to save on $SCRATCH
 cp -R $SLURM_TMPDIR/static/ /network/scratch/d/diazcabm/

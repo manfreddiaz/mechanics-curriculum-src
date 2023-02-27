bbfe501552c4bb3b5ee1fbbae68ab3f413a87d7a
Manfred Diaz <takeitallsource@gmail.com>
2023-02-27 12:52:02 -0500

    Added Sanchez Bergatinos computation

--- src/static/shapley.py
+++ src/static/shapley.py
@@ -1,6 +1,6 @@
 import csv
 import logging
-from itertools import product
+from itertools import permutations, product
 import math
 import os
 from typing import List
@@ -61,8 +61,38 @@ def compute_shapley(values: pd.Series, players: list[str], ordered: bool = False
 def compute_nowak_radzik(values: pd.Series, players: list[str]) -> List[float]:
     return compute_shapley(values, players, True)
 
-def compute_sanchez_bergantinos(df: pd.Series) -> List[float]:
-    pass
+def compute_sanchez_bergantinos(values: pd.Series, players: list[str]) -> List[float]:
+    scaler = preprocessing.MinMaxScaler((-1, 1))
+    n_values = scaler.fit_transform(values.to_numpy().reshape(-1, 1))
+    values.iloc[:, ] = n_values.flatten()
+
+    value = {player: 0.0 for player in players}
+    players_idx = np.arange(len(players))
+    players = np.array(players)
+
+    def backward_dynamics(coalition: list):
+        for player in coalition:
+            next_coalition = list(coalition)
+            next_coalition.remove(player)
+            coalition_id = Coalition.to_id(players[coalition])
+            # log.info((coalition, next_coalition))
+            if len(next_coalition) > 0:
+                next_coalition_id = Coalition.to_id(players[next_coalition])
+                value[players[player]] += values[coalition_id] - values[next_coalition_id]
+            else:
+                value[players[player]] += values[coalition_id]
+            
+            backward_dynamics(next_coalition)
+
+
+    for coalition in permutations(players_idx):
+        backward_dynamics(list(coalition))
+
+    norm = math.factorial(players.shape[0]) ** 2
+    value = {player: value * norm ** -1 for player, value in value.items()}
+
+    return value
+
 
 def compute(cfg: DictConfig):
     indir = make_xpt_dir(cfg)

fe4e8f6999fe4977d487eda1aa3ac39ba3630bd2
Manfred Diaz <takeitallsource@gmail.com>
2023-04-18 08:45:51 -0400

    Initial Commit

---
+++ src/learned/duel.py
@@ -0,0 +1,84 @@
+import argparse
+from distutils.util import strtobool
+import functools
+import os
+import random
+import time
+import numpy as np
+
+import gym
+import hydra
+from omegaconf import DictConfig
+import torch
+
+from stable_baselines3.common.monitor import Monitor
+from SMPyBandits.Policies import Exp3, Exp3S, UCB, Hedge
+
+from learned.utils import eval_agent, hydra_custom_resolvers
+
+from learned.wrappers import FixedEvaluatorWrapper
+
+
+hydra_custom_resolvers()
+
+
+def make_meta_agent(cfg: DictConfig, envs):
+    make_meta_agent = hydra.utils.instantiate(cfg.meta.agent)
+    meta_agent = make_meta_agent(
+        envs=envs,
+        hparams=cfg.meta.alg.hparams,
+        device=torch.device(cfg.torch.device)
+    )
+
+    make_meta_alg = hydra.utils.instantiate(cfg.meta.alg)
+    meta_alg_play_fn, meta_alg_optim_fn, _, _ = make_meta_alg(
+        logger=None,
+        device=torch.device(cfg.torch.device),
+        log_every=cfg.run.log_every,
+        log_file_format=None
+    )
+
+    return meta_agent, meta_alg_play_fn, meta_alg_optim_fn
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="main")
+def main(
+    cfg: DictConfig
+) -> None:
+    random.seed(cfg.run.seed)
+    np.random.seed(cfg.run.seed)
+    torch.manual_seed(cfg.run.seed)
+    torch.backends.cudnn.deterministic = cfg.torch.deterministic
+    
+    epochs = 50000
+    make_meta_task = hydra.utils.instantiate(cfg.meta.task)
+    env = make_meta_task(cfg)
+    env = gym.wrappers.RecordEpisodeStatistics(env)
+    env = Monitor(
+        env,
+        filename=f'{cfg.meta.task.id}/{cfg.main.task.id}/{cfg.main.alg.id}/{cfg.run.seed}',
+        info_keywords=('agent_stats' , 'cf_agent_stats')
+    )
+    env.seed(cfg.run.seed)
+
+    mab = Exp3S(
+        nbArms=env.action_space.n,
+        alpha=1e-5,
+        gamma=0.05,
+    )
+    mab.startGame()
+
+    obs = env.reset()
+    for i in range(epochs):
+        action = mab.choice()
+        obs, reward, done, info = env.step(action)
+        mab.getReward(action, reward)       
+        if done:
+            # print("t: ", mab.trusts)
+            print("r: ", mab.rewards)
+            print("p: ", mab.pulls)
+            env.reset()
+    
+
+if __name__ == '__main__':
+    main()
---
+++ src/learned/factories/meta/duel_cf_meta_trainer_with_index.py
@@ -0,0 +1,42 @@
+
+from gym.wrappers import TimeLimit
+
+from omegaconf import DictConfig
+from ccgm.common.envs.utils import AutoResetWrapper
+from learned.core import (
+    CounterfactualRewardWrapper, CounterfactualSelfPlayWrapper
+)
+from learned.utils import make_meta_env
+from learned.wrappers import FixedEvaluatorWrapper
+
+
+def make_task(
+    id: str,
+    learning_progression: bool,
+    meta_player_id: int,
+    evaluator_action: int,
+    episode_time_limit: int,
+    num_envs: int
+):
+    def make_meta_task(
+        task_config: DictConfig
+    ):
+        env = make_meta_env(task_config, counter_factual=True)
+        env = FixedEvaluatorWrapper(env, eval_action=evaluator_action)
+
+        if episode_time_limit > 0:
+            env = TimeLimit(
+                env,
+                episode_time_limit
+            )
+            env = CounterfactualSelfPlayWrapper(
+                env,
+                learning_progression=learning_progression
+            )
+            env = CounterfactualRewardWrapper(env)
+            env = AutoResetWrapper(
+                env=env
+            )
+        return env
+
+    return make_meta_task
